db37982611443b32a017dc4a53e389c66145e83c
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-06-10 22:04:36 +0300

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,1 +1,21 @@
+# This is a comment explaining the purpose of the code
+
+def calculate_sum(a, b):
+    # This is a function definition with two parameters: a and b
+    result = a + b
+    # This is an assignment statement that calculates the sum of a and b
+    return result
+    # This is a return statement that returns the result
+
+num1 = 5
+num2 = 10
+# These are variable assignments with values 5 and 10
+
+total = calculate_sum(num1, num2)
+# This is a function call to calculate_sum with arguments num1 and num2
+# The result of the function call is assigned to the variable total
+
+print("The sum is:", total)
+# This is a print statement that displays the sum
+
 
